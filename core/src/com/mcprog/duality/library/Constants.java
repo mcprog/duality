16255a052c37b3e39f1909bc07837345ad8a238d
mcprog <michaelcurtis96@gmail.com>
2015-04-25 16:40:51 -0700

    added .gitig and core and desktop with scaling

---
+++ core/src/com/mcprog/duality/library/Constants.java
@@ -0,0 +1,10 @@
+package com.mcprog.duality.library;
+
+/**
+ * Created by mcprog on 4/25/2015.
+ */
+public class Constants {
+
+    public static final int RAW_WIDTH = 856;
+    public static final int RAW_HEIGHT = 482;
+}
