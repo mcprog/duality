ffd5d82562d3365cc62095ec229f994ceb26db65
James Womack <james@the-laboratory.co>
2015-06-10 23:58:56 -0700

    Added return null

--- core/src/com/mcprog/duality/module/FunctionLoader.java
+++ core/src/com/mcprog/duality/module/FunctionLoader.java
@@ -4,9 +4,9 @@ package com.mcprog.duality.module;
  * Created by jameswomack on 6/10/15.
  */
 public class FunctionLoader {
-    public static <T1, T2, TOut> ThreeFunction<T1, T2, TOut> loadFunction(FunctionInfo functionInfo, Class<? super T1> type1,
-                                                                          Class<? super T2> type2, Class<? super TOut> returnType){
-
+    public static <T1, T2, TOut> ThreeFunction<T1, T2, TOut> loadFunction(FunctionInfo functionInfo, Class<? extends T1> type1,
+                                                                          Class<? extends T2> type2, Class<? extends TOut> returnType){
+        
         return null;
     }
 }
