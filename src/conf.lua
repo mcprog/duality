c196ac89e973c72bee6d81da49512a53fcd10647
mcprog <michaelcurtis96@gmail.com>
2015-08-26 18:07:46 -0700

    expierimenting with physics and screenshotting

--- src/conf.lua
+++ src/conf.lua
@@ -4,6 +4,8 @@ function love.conf( t )
 
 	t.window.width = 960
 	t.window.height = 640
+	t.window.minwidth = 800
+	t.window.minheight = 480
 	t.window.resizable = true
 
 	t.title = "Duality pre-Alpha mess"
