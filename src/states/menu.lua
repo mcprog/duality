9d80421d17e1810539d63186ac40d43448053aa8
mcprog <michaelcurtis96@gmail.com>
2015-09-05 13:28:47 -0700

    broken button
    cuz we switching to ruby

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -2,7 +2,8 @@ button = require "ui/button"
 
 local menu = {}
 
-local button1 = button:new(50, 50, 150, 60, {0, 255, 255, 255})
+local button1 = button:new(50, 50, 150, 60, 0, 255, 255, 255)
+local button2 = button:new(250, 50, 150, 60, 0, 0, 255, 255)
 
 function menu:enter(previous)
 	love.graphics.setBackgroundColor(100, 60, 255)
@@ -13,6 +14,7 @@ end
 function menu:draw()
 	love.graphics.print("Duality: Screen: Menu", 0, 0)
 	button1:draw()
+	button2:draw()
 end
 
 function menu:keypressed(key)
@@ -26,6 +28,12 @@ end
 
 function menu:mousepressed(x, y, button)
 	button1:mousepressed(x, y)
+	button2:mousepressed(x, y)
+end
+
+function menu:mousereleased(x, y, button)
+	button1:mousereleased()
+	button2:mousereleased()
 end
 
 return menu
--- src/ui/button.lua
+++ src/ui/button.lua
@@ -3,26 +3,43 @@ local button = {
   y = 0,
   width = 100,
   height = 50,
-  color = {255, 255, 255, 255}
+  r, g, b, a = 255, 255, 255, 255,
+  shadowDiff = 5,
+  buttonPressed = false
 }
 
-function button:new (x, y, width, height, color)
+
+function button:new (x, y, width, height, r, g, b, a)
   setmetatable(button, {})
   self.x, self.y, self.width, self.height = x or self.x, y or self.y, width or self.width, height or self.height
-  self.color = color or self.color
+  self.r, self.g, self.b, self.a = r or self.r, g or self.g, b or self.b, a or self.a
   return self
 end
 
 function button:draw()
-  r, g, b, a = love.graphics.getColor()
-  love.graphics.setColor(self.color)
+  pr, pg, pb, pa = love.graphics.getColor()
+  r, g, b, a = self.r, self.g, self.b, self.a
+  dr, dg, db, da = r * .75, g * .75, b * .75, a
+  love.graphics.setColor(dr, dg, db, da)
+  love.graphics.rectangle("fill", self.x, self.y + self.shadowDiff, self.width, self.height)
+  love.graphics.setColor(r, g, b, a)
   love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-  love.graphics.setColor(r, b, g, a)
+  love.graphics.setColor(pr, pg, pb, pa)
 end
 
 function button:mousepressed(mX, mY)
   if (mX > self.x and mX < self.x + self.width) and (mY > self.y and mY < self.y + self.height) then
-    self.x = self.x + 20
+    self.y = self.y + 5
+    self.buttonPressed = true
+    self.shadowDiff = 0
+  end
+end
+
+function button:mousereleased()
+  if buttonPressed then
+    self.y = self.y - 5
+    self.buttonPressed = false
+    self.shadowDiff = 5
   end
 end
 
