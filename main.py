fb8ec9a2ca0fa17533986601fca10559076ee4b1
mcprog <michaelcurtis96@gmail.com>
2015-09-08 15:17:10 -0700

    pygame initial commit
    just a main file with exit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Duality Pre-Alpha')
+
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+        #print(event)
+        
+
+#uninits pygame
+pygame.quit()
+#exits out of python
+quit()
