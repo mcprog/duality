29b8cffb16eda323dd13817f71938fc099234c58
mcprog <michaelcurtis96@gmail.com>
2016-05-27 22:03:54 -0700

    Using Coronda sdk under lua branch

--- core/src/mcprog/duality/components/BackgroundComponent.java
+++
@@ -1,9 +0,0 @@
-package mcprog.duality.components;
-
-import com.badlogic.ashley.core.Component;
-
-/**
- * Created by mcprog on 10/6/2015.
- */
-public class BackgroundComponent implements Component {
-}
